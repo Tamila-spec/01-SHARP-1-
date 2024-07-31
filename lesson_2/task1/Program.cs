6e0087cfab4535c3b60bee0b7df32ad99fa089a0
Тамила <tamila@Air-Tamila.Dlink>
2024-08-01 00:20:29 +0300

    lesson2 task2

---
+++ lesson_2/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] arr = new int [5]; // выделили память на 5 целых чисел
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 5;
+arr[3] = 4;
+arr[4] = 3;
+
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+int[] arr2 = new int [5]{1, 2, 3, 4, 5};
+
+int arr3 = {1, 2, 3, 4, 5}; // команду на выдел памяти можно не исп, т.к число фиксированное
+
